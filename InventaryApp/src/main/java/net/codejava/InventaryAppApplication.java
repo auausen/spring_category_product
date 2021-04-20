00f8296e99562cd174f0e7d499cccd3c417866cd
naka <ryousen.naka@microworld.co.jp>
2021-04-20 16:28:52 +0900

    form list kansei

---
+++ InventaryApp/src/main/java/net/codejava/InventaryAppApplication.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaryAppApplication.class, args);
+	}
+
+}
